5bbaac535de2cb9bbb9421755307435ac88d7fa7
Safal Sharma <safal.sharma.ss@gmail.com>
2022-09-15 02:46:37 +0545

    make question sets dynamically load from json.

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,16 +1,13 @@
 import 'phaser'
-import Scene from "../Base/Scene";
-import {getRandomItem} from "../utils";
+import Scene from "../Base/Scene"
+import {getRandomItem} from "../utils"
 
 export default class GameScene extends Scene {
     constructor() {
         super("scene_game")
 
-        this.pictures = [
-            {image: 'actor_dog', answer: 'dog'},
-            {image: 'actor_cat', answer: 'cat'}
-        ]
         this.score = 0
+        this.interval = 1500
     }
 
     preload() {
@@ -24,183 +21,147 @@ export default class GameScene extends Scene {
 
         this.load.image('img_star', 'assets/star.png')
 
-        this.load.image('actor_dog', 'assets/dog.png')
-        this.load.image('actor_cat', 'assets/cat.png')
+        this.load.image('dog', 'questions/pictures/dog.png')
+        this.load.image('cat', 'questions/pictures/cat.png')
 
+        this.load.json('questions', '/questions/list.json')
     }
 
     create() {
         super.create()
 
-        this.addBackground('background')
+        this.questions = this.cache.json.get('questions')
 
-        this.showReady()
-        this.btnHome = this.add.image(this.gameWidth * .75, this.gameHeight * .9, 'btn_home')
+        this.background = this.addBackground('background')
+        this.background.setScale(5)
 
-        this.setEvents()
-
-        this.setTimer()
-    }
-
-    setTimer() {
-        this.time.delayedCall(1000, () => {
-            this.readyText.setVisible(false)
-            this.showGo()
-        })
-
-        this.time.delayedCall(2000, () => {
-            this.goText.setVisible(false)
-            // this.cameras.main.flash()
-            this.cameras.main.zoomTo(3, 1000, Phaser.Math.Easing.Cubic.Out)
-
-            this.onGameStart()
-        })
-    }
-
-    setEvents() {
-        this.btnHome.setInteractive()
-
-        this.btnHome.on('pointerover', p => {
-            this.btnHome.scale = 1.1
-        })
-
-        this.btnHome.on('pointerout', p => {
-            this.btnHome.scale = 1
-        })
-
-        this.btnHome.on('pointerup', p => {
-            this.scene.start('scene_menu')
-        })
-    }
-
-    showReady() {
-        let titleTextObj = {
-            content: 'READY?',
-            style: {
-                font: '150px "Press Start 2P"'
-            }
-        }
-        this.readyText = this.addText(titleTextObj, 5, 5)
-    }
-
-    showGo() {
-        let titleTextObj = {
-            content: 'GO!',
-            style: {
-                font: '150px "Press Start 2P"'
-            }
-        }
-        this.goText = this.addText(titleTextObj, 5, 5)
-    }
-
-    onGameStart() {
         let scoreTextObj = {
-            content: this.score,
+            text: this.score,
             style: {
                 font: '40px "Press Start 2P"'
             }
         }
-        this.textScore = this.addText(scoreTextObj, 4.7, 3.9)
+        this.textScore = this.addText(scoreTextObj, 4.9, 1)
 
-        this.imgStar = this.add.image(this.gameWidth * .5, this.gameHeight * .4, 'img_star')
+        this.imgStar = this.addImage('img_star', 5, 1)
+        this.imgStar.displayHeight = 80
+        this.imgStar.displayWidth = 80
+        this.imgStar.setOrigin(0)
 
-        this.btnPause = this.add.image(this.gameWidth * .64, this.gameHeight * .4, 'btn_pause')
+        // this.imgStar = this.add.image(this.w * .5, this.h * .4, 'img_star')
 
-        // this.btnPause.setSize(30, 30)
+        this.btnPause = this.addImage('btn_pause', 9, 1)
 
         this.setGameUIEvents()
         this.startSequence()
     }
 
-    updateScore(score) {
-        this.score = score
-        this.textScore.setText(score)
+
+    updateScore(newScore) {
+        if (newScore < 0) return
+
+        if (this.score < newScore) {
+            if (this.interval > 200) {
+                this.interval = this.interval - (newScore * 10)
+            }
+        } else {
+            if (this.interval < 1500) {
+                this.interval = this.interval + (newScore * 10)
+            }
+        }
+
+        console.log(this.interval)
+
+        this.textScore.setText(newScore)
+        this.score = newScore
     }
 
     startSequence() {
-        let selectedPicture = getRandomItem(this.pictures)
+        let selectedQuestion = getRandomItem(this.questions)
 
-        let picture = this.add.image(this.gameWidth * .5, this.gameHeight * .55, selectedPicture.image).setScale(0.5, 0.5)
+        let picture = this.add.image(this.w * .5, this.h * .55, selectedQuestion.image)
         this.timerText.setText('Ready?')
 
         this.timerText.setOrigin(0.5)
 
-        this.time.delayedCall(1000, () => {
+        this.time.delayedCall(this.interval * .7, () => {
             this.timerText.text = 'GO!'
         })
 
-        this.time.delayedCall(1500, () => {
+        this.time.delayedCall(this.interval, () => {
             picture.setVisible(false)
             this.timerText.text = 'Do you remember?'
-            this.showOptions(selectedPicture)
+            this.showOptions(selectedQuestion)
         })
     }
 
-    showOptions(selectedQuestion) {
-        let option1 = this.add.text(this.gameWidth / 2 - 25, this.gameHeight / 2, 'CAT', {
-            fontSize: '45px',
-            fontStyle: "bold",
-            color: "#f6d200"
-        })
-            .setOrigin(1, 0)
+    showOptions(selectedQuestion = {answerSet: []}) {
 
-        let option2 = this.add.text(this.gameWidth / 2 + 25, this.gameHeight / 2, 'DOG', {
-            fontSize: '45px',
-            fontStyle: "bold",
-            color: "#f6d200"
-        })
+        this.optionsGroup = this.add.group()
 
-
-        option1.setInteractive()
-        option1.on('pointerover', p => {
-            option1.setAlpha(0.5)
-        })
-        option1.on('pointerout', p => {
-            option1.setAlpha(1)
-        })
-        option1.on('pointerdown', p => {
-            if (this.checkAnswer('CAT', selectedQuestion)) {
-                this.updateScore(this.score + 1)
-                this.timerText.setText("Yay!")
-            }else{
-                this.timerText.setText("Incorrect :(")
+        selectedQuestion.answerSet.map((option, i) => {
+            let xPos
+            if (i === 0) {
+                xPos = 5
+            } else if (i % 2 === 0) {
+                xPos = 5 + i
+            } else {
+                xPos = 5 - i
             }
-            this.time.delayedCall(1000, () => {
-                this.startSequence()
-            })
-            option1.destroy()
-            option2.destroy()
-        })
 
-        option2.setInteractive()
-        option2.on('pointerover', p => {
-            option2.setAlpha(0.5)
-        })
-        option2.on('pointerout', p => {
-            option2.setAlpha(1)
-        })
-        option2.on('pointerdown', p => {
-            if (this.checkAnswer('DOG', selectedQuestion)) {
-                this.updateScore(this.score + 1)
-                this.timerText.setText("Yay!")
-            }else{
-                this.timerText.setText("Incorrect :(")
+            let text = this.addText({
+                text: option, style: {
+                    fontSize: '120px',
+                    fontStyle: "bold",
+                    color: "#f6d200"
+                }
+            }, xPos, 5)
+
+            if (i === 0) {
+                text.x = this.w / 2
+                text.x -= (text.width / 2)
+            } else if (i % 2 === 0) {
+                text.setOrigin(0, 0)
+
+            } else {
+                text.setOrigin(1, 0)
             }
-            this.time.delayedCall(1000, () => {
-                this.startSequence()
+
+            text.setInteractive()
+            text.on('pointerover', p => {
+                text.setAlpha(0.5)
             })
-            option1.destroy()
-            option2.destroy()
+            text.on('pointerout', p => {
+                text.setAlpha(1)
+            })
+            text.on('pointerdown', p => {
+                this.checkAnswer(text.text, selectedQuestion)
+            })
+
+            this.optionsGroup.add(text)
         })
     }
 
     checkAnswer(answer, selectedQuestion) {
-        return answer.toLowerCase() === selectedQuestion.answer.toLowerCase();
+        let result = answer.toLowerCase() === selectedQuestion.answer.toLowerCase()
+
+        if (result) {
+            this.updateScore(this.score + 1)
+            this.timerText.setText("Yay!")
+        } else {
+            this.updateScore(this.score - 1)
+            this.timerText.setText("Incorrect :(")
+        }
+
+        this.time.delayedCall(1000, () => {
+            this.startSequence()
+        })
+        this.optionsGroup.destroy(true, true)
     }
 
     setGameUIEvents() {
-        this.timerText = this.add.text(this.gameWidth * .5, this.gameHeight * .64, 'Ready?')
 
+        this.showReady()
         this.btnPause.setInteractive()
 
         this.btnPause.on('pointerover', p => {
@@ -214,5 +175,17 @@ export default class GameScene extends Scene {
         this.btnPause.on('pointerup', p => {
             this.scene.start('scene_menu')
         })
+    }
+
+
+    showReady() {
+        if (this.timerText !== undefined) this.timerText.destroy()
+        this.timerText = this.addText({
+            text: "Ready?", style: {
+                font: '80px'
+            }
+        }, 5, 8)
+        this.positionManager.centerX(this.timerText)
+
     }
 }
