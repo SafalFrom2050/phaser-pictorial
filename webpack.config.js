39c0209e0fba8c9b0e42e9688131a59517056873
Safal Sharma <safal.sharma.ss@gmail.com>
2022-09-15 02:45:59 +0545

    set `mode: development` in webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const path = require('path');
 module.exports = {
 
     entry: './src/index.js',
-
+    mode: "development",
     output: {
         path: path.resolve(__dirname, 'build'),
         publicPath: '/build/',
