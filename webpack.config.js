70d52be145024524a33152913b9554eb986e7dde
Safal Sharma <safal.sharma.ss@gmail.com>
2022-09-15 14:47:29 +0545

    file path changes.

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const path = require('path');
 module.exports = {
 
     entry: './src/index.js',
+    mode: 'production',
     output: {
         path: path.resolve(__dirname, 'build'),
         publicPath: '/build/',
